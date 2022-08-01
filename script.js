fb9630c051694322c3b9dd0c8770f9550578669b
Hodman Ahmed <hodman003@Hodmans-MacBook-Air.local>
2022-07-31 21:13:58 -0500

    First commit

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-var StartBTN = document.querySelector(".Start-Button");
-var QuizRules = document.querySelector(".Quiz-Info")
-//yo
+var startBtn = document.querySelector(".start-button");
+var endGameBtn = document.querySelector(".End-Game-Button");
+var quizRules = document.querySelector(".Quiz-Info")
 var timerClass = document.querySelector(".Timer")
 var timerSec = document.querySelector(".Timer-Sec")
-var timer;
 var timerCount = 60
-var TotalScore = 0
+var totalScore = 0
 var questionCounter = 0
 var questionsQuiz = [{
     question: "What operator is used to access arrays (1)",
@@ -27,30 +26,43 @@ var questionsQuiz = [{
     answers: ["Brackets", "Quotations", "Parentheses", "Period"],
     correct: "Brackets"
 }]
-function endgame() {
+var results = []
 
-    timerClass.remove()
+timerSec.textContent = timerCount
+startBtn.addEventListener("click", startTimer)
 
-}
+//Function to start timer
 function startTimer() {
+    quizRules.style.display = 'none'
     showQuiz()
-    timer = setInterval(function () {
+    setInterval(function () {
         timerCount--;
         timerSec.textContent = timerCount
-        if (timerCount <= 0) {
+        if (timerCount === 0) {
             endgame()
         }
     }, 1000);
-
-    QuizRules.remove()
 }
 
+//Function for Quiz Questions and Answers Buttons
 function showQuiz() {
+    var startButtonNotVisible = document.getElementById('start-button').style.display
+    if (startButtonNotVisible === 'none') {
+        console.log("here")
+        document.querySelector('.high-scores').style.display = 'none'
+        document.querySelector('.all-done').style.display = 'none'
+        document.querySelector('.start-button').style.display = 'block'
+        quizRules.style.display = 'block'
+    }
+    // var highScoresSection = document.getElementById('high-scores')
+    // if (highScoresSection) {
+    //     highScoresSection.style.display = 'none'
+    // }
     var questionEl = document.querySelector(".questions");
     var ele = document.querySelector(".h5q")
     if (ele) ele.remove()
     var h5El = document.createElement("h5")
-    h5El.textContent = questionsQuiz[questionCounter].question
+    if (questionCounter < questionsQuiz.length) h5El.textContent = questionsQuiz[questionCounter].question
     h5El.classList.add("h5q")
 
     questionEl.append(h5El)
@@ -61,45 +73,103 @@ function showQuiz() {
             Answers1[i].remove()
         }
     }
-    for (var i = 0; i < questionsQuiz[questionCounter].answers.length; i++) {
+    for (var i = 0; i < questionsQuiz.length; i++) {
         var buttonEL = document.createElement("button")
-        buttonEL.textContent = questionsQuiz[questionCounter].answers[i]
+        if (questionCounter < questionsQuiz.length) buttonEL.textContent = questionsQuiz[questionCounter].answers[i]
         buttonEL.classList.add("A1")
-        buttonEL.addEventListener("click", CorrectAnswers)
+        buttonEL.addEventListener("click", correctAnswers)
         answersEl.append(buttonEL)
     }
 
-
-}
-
-StartBTN.addEventListener("click", startTimer)
-
-
-function CorrectAnswers(event) {
-    console.log("btn", event.target.textContent, questionCounter, questionsQuiz.length)
-
-    var UserAnswer = event.target.textContent
-    if (questionsQuiz[questionCounter].correct == UserAnswer) {
-        document.getElementById("check").textContent === CorrectAnswers
-        TotalScore += 5
-    } else {
-        if (questionsQuiz[questionCounter].correct !== UserAnswer) {
-            document.getElementById("check").textContent !== CorrectAnswers
-            timerCount-=10
-        }
-
-    }
-    
-    //Quiz is not finshed
-    if(questionCounter <= questionsQuiz.length){
-        questionCounter++
-        console.log(questionCounter)
-        showQuiz()    
-    } 
-    //Not finshed
-    else {
-        alert("Put a message here.")
-        console.log(questionCounter)
+    if (questionCounter === questionsQuiz.length) {
         endgame()
     }
 }
+
+function endgame() {
+    timerClass.remove()
+    document.getElementById('questions').style.display = 'none'
+    document.getElementById('answers').style.display = 'none'
+    document.getElementById('start-button').style.display = 'none'
+    document.getElementById('all-done').style.display = 'block'
+    // startBtn.textContent = 'All Done'
+    // startBtn.classList.remove("start-button")
+    // startBtn.classList.add('All-Done')
+
+    resultsView()
+}
+
+//Function for selecting answers and removing time for wrong answers 
+function correctAnswers(e) {
+    var userAnswer = e.target.textContent
+    var correctAnswer = questionsQuiz[questionCounter].correct
+    // userAnswer === correctAnswer ? totalScore += 5 : timerCount -=10 <-- ternary operator aka fancy if statement
+    if (userAnswer === correctAnswer) {
+        totalScore += 5
+    } else {
+        timerCount -= 10 
+    }
+
+    //Quiz is not finshed
+    if (questionCounter < questionsQuiz.length) {
+        questionCounter++
+        showQuiz()
+    }
+}
+
+function resultsView() {
+    var initials
+    var contentSection = document.getElementById('content')
+    
+    var totalScoreDiv = document.createElement('div')
+    totalScoreDiv.setAttribute("id", "total-score");
+    totalScoreDiv.textContent = `Your final score is ${totalScore}.`
+    contentSection.appendChild(totalScoreDiv)
+    
+    var textBox = document.createElement("input");
+    textBox.setAttribute("type", "text");
+    textBox.setAttribute("id", "intials");
+    textBox.setAttribute("placeholder", "enter your initials");
+    contentSection.appendChild(textBox)
+    
+    var submitBtn = document.createElement("input");
+    submitBtn.setAttribute("type", "submit");
+    submitBtn.setAttribute("id", "submit-button");
+    contentSection.appendChild(submitBtn)
+
+    textBox.addEventListener('change', (e) => {
+        initials += e.target.value
+    }) // figure why this isnt saving
+    results.push({initials, totalScore})
+    submitBtn.addEventListener('click', highScores)
+}
+
+
+function highScores() {
+    document.getElementById('all-done').style.display = 'none'
+    var highScoresView = document.createElement('section')
+    highScoresView.setAttribute('id', 'high-scores')
+    document.getElementById('content').style.display = 'none'
+    document.body.appendChild(highScoresView)
+
+
+    var scoresList = document.createElement('ol')
+    results.map(score => {
+        var eachScore = document.createElement('li')
+        eachScore.textContent = `${score.initials} ${score.totalScore}`
+        scoresList.appendChild(eachScore)
+
+    })
+    highScoresView.appendChild(scoresList)
+    
+    var restartButton = document.createElement('button')
+    restartButton.setAttribute("id", "restart-button");
+    restartButton.textContent = "Restart"
+    highScoresView.appendChild(restartButton)
+
+    restartButton.addEventListener('click', startTimer)
+}
+
+// we need to add quiz rules
+// add the timer back
+// add the start button back
